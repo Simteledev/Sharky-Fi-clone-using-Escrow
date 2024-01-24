95a4683ab63b1228ab22a88127365da1c61114f1
simteledev <simonokwa7@gmail.ccom>
2024-01-24 22:56:10 +0100

    update address

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("6UpiGhxPDVnsje43Wa9n3PSMLkxt6seQhjVymjwMV32b");
 
 #[program]
 pub mod nft_lend_borrow {
